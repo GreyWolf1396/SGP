c2c8bdaaa272204d2482a4cd8c30b7756cb8860e
Serhii Tereshchenko <steres@softserveinc.com>
2021-03-22 10:33:43 +0200

    feat: added injectorpackage.

---
+++ SGP.GameCreator.Webhost/Infrastructure/DependencyInjection/WebHostPackage.cs
@@ -0,0 +1,16 @@
+﻿using SGP.GameCreator.Webhost.Services;
+using SGP.GameCreator.Webhost.Services.Interfaces;
+using SimpleInjector;
+using SimpleInjector.Packaging;
+
+namespace SGP.GameCreator.Webhost.Infrastructure.DependencyInjection
+{
+    public class WebHostPackage : IPackage
+    {
+        public void RegisterServices(Container container)
+        {
+            container.Register<IGameService, GameService>();
+        }
+    }
+
+}
--- SGP.GameCreator.Webhost/Models/GameModel.cs
+++ SGP.GameCreator.Webhost/Models/GameModel.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace SGP.GameCreator.Webhost.Models
+﻿namespace SGP.GameCreator.Webhost.Models
 {
     public class GameModel
     {
